6a96f72e1f07454760a37654507bdacf84ad6841
Neyan najat <nyannajat8@gmail.com>
2018-07-31 08:02:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
  function BoardMember(name, homeState, training){
   this.name = name
-// \  this.homeState = homeState
-//   this.training = training
-//
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
-//   this.approve = function(){
-//     return 'You can do that!'
+  this.homeState = homeState
+  this.training = training
+   this.veto = function(){
+    return 'No, I must disagree'
+   }
+  this.approve = function(){
+     return 'You can do that!'
 //   }
 //   this.doCharity = function(){
 //     return "I like to help people."
