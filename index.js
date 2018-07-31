9cf5cdf5efa7af1e136e95dc4b57248277e85b0e
Neyan najat <nyannajat8@gmail.com>
2018-07-31 08:01:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
+ function BoardMember(name, homeState, training){
+  this.name = name
+// \  this.homeState = homeState
 //   this.training = training
 //
 //   this.veto = function(){
